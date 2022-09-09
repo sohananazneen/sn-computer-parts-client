b37cbe11d34976572db2e398a45e0e2d0a2f6951
SohanaN <sohananaz01@gmail.com>
2022-09-09 10:21:51 +0600

    register , about

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "./Pages/Home/Home";
 import About from "./Pages/About/About";
 import Login from "./Pages/Login/Login";
 import Footer from "./Pages/Shared/Footer";
+import Register from "./Pages/Login/Register";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
       </Routes>
       <Footer />
     </div>
--- src/Pages/Login/Register.js
+++ src/Pages/Login/Register.js
@@ -2,10 +2,10 @@ import React from 'react';
 import loginGif from '../../assets/images/loginGif.gif'
 import { Link } from 'react-router-dom';
 
-const Login = () => {
+const Register = () => {
     return (
         <div className="hero min-h-screen bg-base-200">
-            <div className="hero-content flex-col lg:flex-row">
+            <div className="hero-content flex-col lg:flex-row-reverse">
                 <div
                     class="grow-0 shrink-1 md:shrink-0 basis-auto xl:w-6/12 lg:w-6/12 md:w-9/12 mb-12 md:mb-0"
                 >
@@ -19,6 +19,12 @@ const Login = () => {
                     <div className="card-body">
                         <div className="form-control">
                             <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" placeholder="name" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
                             <input type="text" placeholder="email" className="input input-bordered" />
@@ -28,17 +34,14 @@ const Login = () => {
                                 <span className="label-text">Password</span>
                             </label>
                             <input type="text" placeholder="password" className="input input-bordered" />
-                            <label className="label">
-                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                            </label>
                         </div>
                         <div className="form-control mt-6">
-                            <button className="btn btn-primary">Login</button>
+                            <button className="btn btn-primary">Register</button>
                             <p class="text-sm text-white font-semibold mt-2 pt-1 mb-0">
-                                Don't have an account?
-                                <Link to="/register"
+                                Already Registered?
+                                <Link to="/login"
                                     class="text-red-600 hover:text-red-700 focus:text-red-700 transition duration-200 ease-in-out"
-                                > Register </Link>
+                                > Login </Link>
                             </p>
                         </div>
                     </div>
@@ -48,4 +51,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Register;
